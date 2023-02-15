ce89f7ccb03678ce4dc407cd900688989e751b1a
Igone van_der_Kley <igone@vdkley.nl>
2023-02-15 08:02:16 +0100

    db re-expo

--- db/crud-php-pdo-opdracht03..sql
+++ db/crud-php-pdo-opdracht03..sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 10 feb 2023 om 16:34
+-- Gegenereerd op: 15 feb 2023 om 07:00
 -- Serverversie: 5.7.36
 -- PHP-versie: 8.0.2
 
@@ -33,12 +33,23 @@ CREATE TABLE IF NOT EXISTS `achtbaan` (
   `NaamAchtbaan` varchar(50) DEFAULT NULL,
   `NaamPretpark` varchar(50) DEFAULT NULL,
   `Land` varchar(50) DEFAULT NULL,
-  `Topsnelheid` varchar(50) DEFAULT NULL,
+  `Topsnelheid` int(11) DEFAULT NULL,
   `Hoogte` int(11) DEFAULT NULL,
   `Datum` date DEFAULT NULL,
-  `Cijfer` decimal(10,0) DEFAULT NULL,
+  `Cijfer` decimal(4,2) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `achtbaan`
+--
+
+INSERT INTO `achtbaan` (`Id`, `NaamAchtbaan`, `NaamPretpark`, `Land`, `Topsnelheid`, `Hoogte`, `Datum`, `Cijfer`) VALUES
+(1, 'Red Force', 'Ferrari Land', 'Spanje', 160, 112, '1968-03-02', '9.20'),
+(2, 'Ring Racer', 'Circuit Nürnberg', 'Duitsland', 160, 110, '1974-08-01', '8.70'),
+(3, 'Hyperion', 'EnergyLandia ', 'Polen', 141, 77, '2009-09-09', '6.30'),
+(4, 'Furios Baco', 'PortAventura', 'Spanje', 138, 23, '2018-06-06', '5.50'),
+(5, 'Shambala', 'PortAventura', 'Spanje', 134, 102, '2017-04-03', '9.70');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
