a39054c561ffcfbf85c660c3b3bfb11d479ddc73
Igone van_der_Kley <igone@vdkley.nl>
2023-02-15 09:13:03 +0100

    altered read.php

--- read.php
+++ read.php
@@ -54,7 +54,7 @@ foreach ($result as $info) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PIZZA</title>
+    <title>achtbanen</title>
 </head>
 
 <body>
